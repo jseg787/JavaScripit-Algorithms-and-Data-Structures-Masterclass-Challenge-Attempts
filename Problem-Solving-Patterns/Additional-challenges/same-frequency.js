043f29ed59eb09dcc8fa9a45d033657dd22c34f9
jseg787 <jsegarra3@gmail.com>
2019-05-24 12:14:47 -0400

    fixed small formatting issue

--- Problem-Solving-Patterns/Additional-challenges/same-frequency.js
+++ Problem-Solving-Patterns/Additional-challenges/same-frequency.js
@@ -11,17 +11,17 @@ sameFrequency(3589587, 5879385);
 sameFrequency(22, 222);
 */
 
-function sameFrequency(num1, num2){
+function sameFrequency(num1, num2) {
     // good luck. Add any arguments you deem necessary.
     let str1 = num1.toString();
     let str2 = num2.toString();
     if (str1.length !== str2.length) {
         return false;
     }
-    
+
     let strFreq1 = {};
     let strFreq2 = {};
-    
+
     // count the frequencies of the letters and store them as properties
     for (let letter of str1) {
         strFreq1[letter] = (strFreq1[letter] || 0) + 1;
@@ -29,7 +29,7 @@ function sameFrequency(num1, num2){
     for (let letter of str2) {
         strFreq2[letter] = (strFreq2[letter] || 0) + 1;
     }
-    
+
     // for each key of the object, check if the values are the same
     for (let key in strFreq1) {
         if (strFreq1[key] !== strFreq2[key]) {
@@ -37,5 +37,4 @@ function sameFrequency(num1, num2){
         }
     }
     return true;
-  }
-  
+}
