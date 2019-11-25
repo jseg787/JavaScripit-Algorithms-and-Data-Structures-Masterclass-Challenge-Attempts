844fbbdb7acb0f9a6eaadbc883240bdff42dc224
Jseg787 <jsegarra3@gmail.com>
2019-11-25 17:35:23 -0500

    add doubly linked list code

---
+++ doubly-linked-lists/doubly_linked_list.js
@@ -0,0 +1,112 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    const newNode = new Node(val);
+    if (this.length === 0) {
+      // if the list is empty make the head and tail the new
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      // otherwise make tail.next the new node, make the newNode
+      // prev be the current tail, then update the tail to be the
+      // newNode
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    // return the DoublyLinkedList
+    return this;
+  }
+
+  pop() {
+    if (this.length === 0) {
+      return undefined;
+    }
+
+    const poppedNode = this.tail;
+
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      // update the tail to be the tails prev
+      this.tail = poppedNode.prev;
+      this.tail.next = null;
+      poppedNode.prev = null;
+    }
+    this.length--;
+    return poppedNode;
+  }
+
+  shift() {
+    if (this.length === 0) {
+      return undefined;
+    }
+
+    const shiftedNode = this.head;
+
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = shiftedNode.next;
+      this.head.prev = null;
+      shiftedNode.next = null;
+    }
+    this.length--;
+    return shiftedNode;
+  }
+
+  unshift(val) {
+    const newNode = new Node(val);
+
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.head.prev = newNode;
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  get(index) {
+    if (index < 0 || index >= this.length) {
+      return null;
+    }
+    let count, current;
+
+    if (index <= this.length / 2) {
+      count = 0;
+      current = this.head;
+      while (count !== index) {
+        current = current.next;
+        count++;
+      }
+    } else {
+      count = this.length - 1;
+      current = this.tail;
+      while (count !== index) {
+        current = current.prev;
+        count--;
+      }
+    }
+    return current;
+  }
+}
