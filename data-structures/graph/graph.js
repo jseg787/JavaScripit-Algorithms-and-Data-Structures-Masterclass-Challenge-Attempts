815c7369b43c94d103e318cb102f1172812905c1
jseg787 <jsegarra3@gmail.com>
2020-12-13 14:36:31 -0500

    add iterative depth-first graph traversal

--- data-structures/graph/graph.js
+++ data-structures/graph/graph.js
@@ -55,6 +55,29 @@ class Graph {
 
 		return results;
 	}
+
+	DFSIterative(start) {
+		const stack = [ start ];
+		const result = [];
+		const visited = {};
+		let currentVertex;
+
+		visited[start] = true;
+
+		while (stack.length) {
+			currentVertex = stack.pop();
+			result.push(currentVertex);
+
+			this.adjacencyList[currentVertex].forEach((neighbor) => {
+				if (!visited[neighbor]) {
+					visited[neighbor] = true;
+					stack.push(neighbor);
+				}
+			});
+		}
+
+		return result;
+	}
 }
 
 const g = new Graph();
@@ -73,3 +96,6 @@ g.addEdge('C', 'E');
 g.addEdge('D', 'E');
 g.addEdge('D', 'F');
 g.addEdge('E', 'F');
+
+console.log(g.DFSRecursive('A'));
+console.log(g.DFSIterative('A'));
